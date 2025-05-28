6d5471e1aadf788ff0aff1a1f8d1f58dddc6165e
RubiGeek <carlos.m.delgado@gmail.com>
2025-05-28 14:21:33 +0200

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,14 +6,13 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5b7aefd8f2820028b3a0956a4f4b76a1aa88da1a
+AESD_ASSIGNMENTS_VERSION = '5b7aefd8f2820028b3a0956a4f4b76a1aa88da1a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-RubiGeek.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
-AESD_ASSIGNMENTS_DEPENDENCIES = host-pkgconf
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
