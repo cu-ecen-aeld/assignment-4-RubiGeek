55733c634d152fb20da06d15527a3af59b576289
RubiGeek <carlos.m.delgado@gmail.com>
2025-06-02 13:25:53 +0200

    Assignment 5 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '5b7aefd8f2820028b3a0956a4f4b76a1aa88da1a'
+AESD_ASSIGNMENTS_VERSION = '4751bbe29d57ed665de93ea76693b8674392d7e3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,10 +16,13 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0775 $(@D)/server/aesdsocket $(TARGET_DIR)/bin
+	$(INSTALL) -m 0775 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -d 0775 $(@D)/finder-app/ $(TARGET_DIR)/bin
 	$(INSTALL) -m 0775 $(@D)/finder-app/* $(TARGET_DIR)/bin
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
