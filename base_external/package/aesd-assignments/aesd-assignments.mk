e196c433f0c5b8d583ef703475b1fd3fd963befe
RubiGeek <carlos.m.delgado@gmail.com>
2025-06-02 15:18:07 +0200

    Assignment 5 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4751bbe29d57ed665de93ea76693b8674392d7e3'
+AESD_ASSIGNMENTS_VERSION = 'bbe761c74d113e592d511b49d4d8c353f9cbf9ad'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
