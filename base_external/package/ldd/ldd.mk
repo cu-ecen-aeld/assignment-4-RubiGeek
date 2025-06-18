2021dba7b15648003045f105fabee8ce7cb87bb3
RubiGeek <carlos.m.delgado@gmail.com>
2025-06-18 13:49:15 +0200

    Assignment 7 part 2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -10,7 +10,7 @@ LDD_VERSION = 'd473accb3301a1d2c1527ca589aa160f4c0cbf5a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = 'git@github.com:cu-ecen-aeld/cu-ecen-aeld/assignment-7-RubiGeek.git'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-RubiGeek.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = misc-modules
